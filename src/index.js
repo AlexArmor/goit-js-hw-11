94b7e8f5c1be739298865d2a41bd9ceba62c8008
AlexArmor <lexlutermotion@gmail.com>
2022-11-21 23:29:40 +0200

    4th commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { PixabayApi } from './pixabay-api';
 
 const searchFormEl = document.querySelector('#search-form');
+const galleryEl = document.querySelector('.gallery');
 
 const pixabayApi = new PixabayApi;
 
@@ -13,8 +14,31 @@ const onSearchFormSubmit = event => {
     pixabayApi.searchQuery = event.target.elements.searchQuery.value;
     pixabayApi
         .fetchPhotos()
-        .then(data => { console.log(data); })
+        .then(renderMarkup)
         .catch(err => { console.log(err); });
+}
+
+function renderMarkup(promiseArray, position = 'beforeend') {
+    const markup = promiseArray.map(({webformatURL, tags, likes, views, comments, downloads}) => {
+        return `<div class="photo-card">
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+        <div class="info">
+          <p class="info-item">
+            <b>Likes${likes}</b>
+          </p>
+          <p class="info-item">
+            <b>Views${views}</b>
+          </p>
+          <p class="info-item">
+            <b>Comments${comments}</b>
+          </p>
+          <p class="info-item">
+            <b>Downloads${downloads}</b>
+          </p>
+        </div>
+      </div>`
+    }).join('');
+        galleryEl.insertAdjacentHTML(position, markup);
 }
 
 searchFormEl.addEventListener('submit', onSearchFormSubmit);

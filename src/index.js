3edec66abf2c9609020ee3c0fa0c7ddc6399860b
Oleksandr Bron <lexlutermotion@gmail.com>
2022-11-22 12:21:57 +0200

    6th commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import './css/styles.css';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
@@ -9,36 +10,36 @@ const galleryEl = document.querySelector('.gallery');
 const pixabayApi = new PixabayApi;
 
 const onSearchFormSubmit = event => {
-    event.preventDefault();
+  event.preventDefault();
 
-    pixabayApi.searchQuery = event.target.elements.searchQuery.value;
-    pixabayApi
-        .fetchPhotos()
-        .then(renderMarkup)
-        .catch(err => { console.log(err); });
+  pixabayApi.searchQuery = event.target.elements.searchQuery.value;
+  pixabayApi
+    .fetchPhotos()
+    .then(renderMarkup)
+    .catch(err => { console.log(err); });
 }
 
 function renderMarkup(promiseArray, position = 'beforeend') {
-    const markup = promiseArray.hits.map(({webformatURL, tags, likes, views, comments, downloads}) => {
-        return `<div class="photo-card">
+  const markup = promiseArray.hits.map(({ webformatURL, tags, likes, views, comments, downloads }) => {
+    return `<div class="photo-card">
         <img src="${webformatURL}" alt="${tags}" loading="lazy" />
         <div class="info">
           <p class="info-item">
-            <b>Likes${likes}</b>
+            <b>Likes</b><span>${likes}</span>
           </p>
           <p class="info-item">
-            <b>Views${views}</b>
+            <b>Views</b><span>${views}</span>
           </p>
           <p class="info-item">
-            <b>Comments${comments}</b>
+            <b>Comments</b><span>${comments}</span>
           </p>
           <p class="info-item">
-            <b>Downloads${downloads}</b>
+            <b>Downloads</b><span>${downloads}</span>
           </p>
         </div>
       </div>`
-    }).join('');
-        galleryEl.insertAdjacentHTML(position, markup);
+  }).join('');
+  galleryEl.insertAdjacentHTML(position, markup);
 }
 
 searchFormEl.addEventListener('submit', onSearchFormSubmit);
